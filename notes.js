8feb4edeebb8a631881f87f0e95903b27481b320
averynoble <averynoble.an@gmail.com>
2021-05-27 13:19:53 -0500

    daily notes passing objects in array practice

---
+++ notes.js
@@ -0,0 +1,35 @@
+const notes = [
+    {
+        id: 1,
+        subject: "HTML",
+        date: "05/20/21",
+        feeling: "Excited",
+        timeSpent: 240
+    },
+
+    {
+        id: 2,
+        subject: "Cascading Style Sheets",
+        date: "05/24/21",
+        feeling: "Anxious",
+        timeSpent: 1200
+    },
+
+    {
+        id: 3,
+        subject: "Git",
+        date: "05/24/21",
+        feeling: "Nervous",
+        timeSpent: 900
+    },
+
+    {
+        id: 4,
+        subject: "JavaScript",
+        date: "05/26/21",
+        feeling: "Overwhelmed",
+        timeSpent: 120
+    }
+]
+
+console.log(notes);
